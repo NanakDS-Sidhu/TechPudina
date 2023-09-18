3f9faeaac3cbb392f2a456b000c1ecd474f0ea92
anshRS <anshraj0426@gmail.com>
2023-09-19 01:34:07 +0530

    Minor bug fixes

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <Image src={HomeImg}></Image>
+          <Image src={HomeImg} alt="home background image"></Image>
         </div>
       </div>
 
