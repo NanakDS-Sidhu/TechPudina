4a1142c76d6eff7be03b3e8d4e24d9684f58d4e1
nanak <sidhu.nds22@gmail.com>
2023-09-18 17:49:40 +0530

    Select Section

---
+++ app/ProfessionCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function ProfessionCard(props) {
+  return (
+    <div className='w-56 text-center text-black'>
+    <div className='bg-white rounded-lg h-56 my-2 flex justify-center items-center text-9xl'>
+        {props.icon}
+    </div>
+    <h2><b>{props.children}</b></h2>
+</div>
+  )
+}
