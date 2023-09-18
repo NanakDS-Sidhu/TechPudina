1dade832a49648d455069b5ad2b62a915540be30
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-09-18 21:08:40 +0530

    Minor Changes

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import HomeImg from "../public/assets/home-bg.jpg";
 import ProfessionCard from "../components/ProfessionCard";
 
 import { PiScalesThin } from "react-icons/pi";
+import {HiOutlineDocumentText} from "react-icons/hi";
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
             <div className="flex border border-purple-200 rounded">
               <input
                 type="text"
-                className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-4 py-2 text-black bg-white border rounded-md focus:border-black  focus:outline-none focus:ring "
                 placeholder="Search..."
               />
               <button className="px-4 text-white bg-black border-l rounded ">
@@ -46,11 +47,11 @@ export default function Home() {
           Select a Service
         </h1>
         <div className="flex justify-around h-1/2">
-          <ProfessionCard icon={<PiScalesThin />}>Advocates</ProfessionCard>
-          <ProfessionCard icon={<PiScalesThin />}>Document Writers</ProfessionCard>
-          <ProfessionCard icon={<PiScalesThin />}>Notaries</ProfessionCard>
-          <ProfessionCard icon={<PiScalesThin />}>Mediators</ProfessionCard>
-          <ProfessionCard icon={<PiScalesThin />}>Arbitrators</ProfessionCard>
+          <button><ProfessionCard icon={<PiScalesThin />}>Advocates</ProfessionCard></button>
+          <button><ProfessionCard icon={<HiOutlineDocumentText />}>Document Writers</ProfessionCard></button>
+          <button><ProfessionCard icon={<PiScalesThin />}>Notaries</ProfessionCard></button>
+          <button><ProfessionCard icon={<PiScalesThin />}>Mediators</ProfessionCard></button>
+          <button><ProfessionCard icon={<PiScalesThin />}>Arbitrators</ProfessionCard></button>
         </div>
       </div>
     </>
