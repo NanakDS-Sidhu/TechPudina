4ca430a47465c1fc802d95ecf4064ed576709265
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-09-18 18:05:02 +0530

    Designed home page

--- components/ProfessionCard.js
+++ components/ProfessionCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProfessionCard(props) {
   return (
     <div className='w-56 text-center text-black'>
-    <div className='bg-white rounded-lg h-56 my-2 flex justify-center items-center text-9xl'>
+    <div className='bg-white rounded-lg drop-shadow-lg h-56 my-2 flex justify-center items-center text-9xl'>
         {props.icon}
     </div>
     <h2><b>{props.children}</b></h2>
