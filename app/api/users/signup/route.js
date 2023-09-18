2f76730eab1d1992f1558404774994d43f9f691a
anshRS <anshraj0426@gmail.com>
2023-09-18 20:10:25 +0530

    Developed authentication api

---
+++ app/api/users/signup/route.js
@@ -0,0 +1,42 @@
+import { connectDB } from "@/config/dbConfig";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connectDB()
+
+export async function POST(request){
+    try {
+        const reqBody = await request.json()
+        const {email, password, fullName, role} = reqBody
+
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+            role
+        })
+
+        const savedUser = await newUser.save()             
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        })     
+
+    } catch (error) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
