e8c8fc31baf1d2dd854e17c12f9d922ff94e2e63
anshRS <anshraj0426@gmail.com>
2023-09-19 08:39:17 +0530

    Created routes for home cards and fetched provider data

--- app/service/[service]/page.js
+++ app/service/[service]/page.js
@@ -4,18 +4,18 @@ import DropDownFilter from './DropDownFilter';
 import Filters from './Filters';
 
 
-export default function Page() {
+export default function Page({params}) {
 
   return (
     <div className='h-screen  text-black flex'>
       <div className='w-1/4 h-screen'>
-        <h2 className='text-5xl p-4'>Filters</h2>
+        <h2 className='text-4xl font-bold p-4'>Filters</h2>
         <Filters></Filters>
 
       </div>
 
-      <div className='grow' >      <h1 className='text-5xl p-4'>List of Lawyers</h1>
-        <PersonTable/>
+      <div className='grow' >      <h1 className='text-4xl font-bold p-4'>List of {params.service}</h1>
+        <PersonTable role={params.service}/>
       </div>
 
     </div>
